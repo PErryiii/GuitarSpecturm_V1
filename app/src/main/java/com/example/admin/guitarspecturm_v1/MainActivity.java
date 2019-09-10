f48376751a9c656a5932bbc1d38b980f0ff853a0
Perry <1195008365@qq.com>
2019-09-10 11:56:01 +0800

    1

--- app/src/main/java/com/example/admin/guitarspecturm_v1/MainActivity.java
+++ app/src/main/java/com/example/admin/guitarspecturm_v1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.guitarspecturm_v1;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
@@ -10,19 +12,29 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView text;
+    private LrcView lrcView;
+    private MediaPlayer player;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String lrcString = "[00:00.10]Hard Love - Usher" +
-                "[00:00.20]Written by:Bibi Bourelly/Raymond IV" +
+        text = (TextView) findViewById(R.id.text);
+
+        String lrcString = "[00:00.10]Hard Love - Usher\n" +
+                "[00:00.20]Written by:Bibi Bourelly/Raymond IV\n" +
                 "[00:00.30]";
-        LrcParser lrcParser = new LrcParser();
-        List<LrcBean> lrcBeanList = new ArrayList<>();
-        lrcBeanList = lrcParser.parseStr2Bean(lrcString);
+        /*LrcParser lrcParser = new LrcParser();
+        List<LrcBean> lrcBeanList = lrcParser.parseStr2Bean(lrcString);
         for (int i = 0; i < lrcBeanList.size(); i++){
-            System.out.println(i);
-        }
+            text.setText(text.getText() + "\n" + lrcBeanList.get(i).getLrc());
+        }*/
+
+        lrcView = (LrcView) findViewById(R.id.lrcView);
+        lrcView.setLrc(lrcString);
+        lrcView.setPlayer(player);
+        lrcView.init();
     }
 }
